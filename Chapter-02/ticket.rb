1ff6ae273937233807bb2876aa6b8831b40c93f2
jacobrees <jacob.rees97@yahoo.com>
2020-06-30 11:58:58 +0100

    Add ticket object and stored date within object to print ticket details after ticket.rb file

---
+++ Chapter-02/ticket.rb
@@ -0,0 +1,31 @@
+ticket = Object.new
+
+def ticket.date
+    "1903-01-02"
+end
+def ticket.venue
+    "Town Hall"
+end
+def ticket.event
+    "Authors Reading"
+end
+def ticket.performer
+    "Mark Twain"
+end
+def ticket.seat
+    "Second Balcony, Row J, seat 12"
+end
+def ticket.price
+    "5.50"
+end
+
+print "This ticket is for: "
+print ticket.event + ", at "
+print ticket.venue + ", on "
+puts ticket.date + "."
+print "The performer is "
+puts ticket.performer + "."
+print "The seat is "
+print ticket.seat + ", "
+print "and it costs $"
+puts "%.2f." % ticket.price
