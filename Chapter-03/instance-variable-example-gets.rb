9410e46c7ce32caf335be7fb528351b880f999b2
jacobrees <jacob.rees97@yahoo.com>
2020-07-02 11:41:29 +0100

    Add capitalize method to returned string inside of set_name method

--- Chapter-03/instance-variable-example-gets.rb
+++ Chapter-03/instance-variable-example-gets.rb
@@ -2,7 +2,7 @@ class Person
     def set_name
         puts "Enter person's name..."
         string = gets.chomp 
-        @name = string        
+        @name = string.capitalize        
     end
     def get_name
         puts "Returning person's name..."
