e768a0cf9f62c77d335ca293110a82c619b2c7fd
jacobrees <jacob.rees97@yahoo.com>
2020-07-04 11:52:19 +0100

    create an instance of class Stack, and did examples of using methods from the stacklike module to add and remove items from instance of Stack Class

--- Chapter-04/stack.rb
+++ Chapter-04/stack.rb
@@ -3,3 +3,15 @@ require_relative "stacklike"
 class Stack
     include Stacklike  # Mixing the Stacklike Module into the Stack Class
 end
+
+s = Stack.new
+s.add_to_stack("item one")
+s.add_to_stack("item two")
+s.add_to_stack("item three")
+puts "Objects currently on the stack:"
+puts s.stack
+taken = s.take_from_stack
+puts "Removed this object:"
+puts taken
+puts "Now on stack:"
+puts s.stack
