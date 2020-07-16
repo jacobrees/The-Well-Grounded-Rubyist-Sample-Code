140c0e2cb403e3276f0776fb6d49797e8eb572e2
jacobrees <jacob.rees97@yahoo.com>
2020-07-16 10:16:13 +0100

    Add call to_a on a Struct example in Chapter 7

---
+++ Chapter-07/to_a_struct.rb
@@ -0,0 +1,5 @@
+Computer = Struct.new(:os, :manufacturer)
+laptop1 = Computer.new("linux", "Lenovo")
+laptop2 = Computer.new("os x", "Apple")
+laptop_arr = [laptop1, laptop2].map { |laptop| laptop.to_a}
+puts laptop_arr
