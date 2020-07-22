431204c041705514100a178d9c8960f9afd56ad6
jacobrees <jacob.rees97@yahoo.com>
2020-07-22 11:20:53 +0100

    Create rainbow.rb for Chapter-10 as an example of how to create and Enumerable class and its deployment of each method

---
+++ Chapter-10/rainbow.rb
@@ -0,0 +1,17 @@
+class Rainbow
+    include Enumerable
+    def each
+        yield "red"
+        yield "orange"
+        yield "yellow"
+        yield "green"
+        yield "blue"
+        yield "indigo"
+        yield "violet"
+    end
+end
+
+r = Rainbow.new
+r.each do |color|
+    puts "Next color: #{color}"
+end
